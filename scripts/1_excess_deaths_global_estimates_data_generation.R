4ea0b26c142e0e0140ad3fd3ab3a8dcc2b03e137
Sondre Ulvund Solstad <sondresolstad@economist.com>
2021-11-03 15:44:16 +0000

    added temporal buffer to reported excess deaths data import to account for possible issues in data less than 2 weeks old

--- scripts/1_excess_deaths_global_estimates_data_generation.R
+++ scripts/1_excess_deaths_global_estimates_data_generation.R
@@ -42,8 +42,9 @@ daily_excess_deaths <- expand.grid(date = seq(min(excess_deaths_source$date), as
   drop_na(-date, -iso3c) %>% 
   filter(!iso3c %in% c("ARM","AZE","ETH","MMR")) # Removes countries which have entered (possibly civil) wars
 
-# Removing very recent data for countries with clear reporting lag issues (Malaysia) after discussions with our source for this data:
-daily_excess_deaths <- daily_excess_deaths[!(daily_excess_deaths$iso3c == "MYS" & daily_excess_deaths$date >= Sys.Date()-30*5), ]
+# Removing very recent data for countries with clear reporting lag issues (Malaysia) after discussions with our source for this data, as well as data that is less than one week old (also to avoid reporting lag issues):
+daily_excess_deaths <- daily_excess_deaths[!(daily_excess_deaths$iso3c == "MYS" & daily_excess_deaths$date >= Sys.Date()-30*5) & daily_excess_deaths$date < Sys.Date()-2*7, ]
+
 
 # Check to ensure and remove any duplicated country-dates
 daily_excess_deaths <- daily_excess_deaths[!duplicated(paste0(daily_excess_deaths$iso3c, "_", daily_excess_deaths$date)), ]
