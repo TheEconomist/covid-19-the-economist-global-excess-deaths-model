c8230908f189b5c62308ab49f13f52832bb7011e
Sondre Ulvund Solstad <sondresolstad@economist.com>
2022-02-08 22:40:22 +0000

    tweaked documentation to data generation script

--- scripts/0_excess_deaths_global_estimates_autoupdater.R
+++ scripts/0_excess_deaths_global_estimates_autoupdater.R
@@ -147,7 +147,7 @@ library(agtboost)
 m_predictors <- readRDS("output-data/model-objects/m_predictors.RDS")
 
 # Define number of bootstrap iterations. We use 200.
-B = 120
+B = 100
 counter = 0
 
 # Define ensemble size for central estimate
