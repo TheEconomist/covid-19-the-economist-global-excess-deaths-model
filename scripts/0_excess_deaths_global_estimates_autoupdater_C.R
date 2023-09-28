f2566caf7f0237d353ba6457783691d198e37cfa
Sondre Ulvund Solstad <sondresolstad@economist.com>
2023-09-28 13:09:06 +0100

    offline update

--- scripts/0_excess_deaths_global_estimates_autoupdater_C.R
+++ scripts/0_excess_deaths_global_estimates_autoupdater_C.R
@@ -92,7 +92,7 @@ generate_model_loop(
   Y_full = Y[!is.na(Y)],   # Defines outcome variable
   B = 1, 
   include_main_estimate = T,
-  main_estimate_learning_rate = 0.001,
+  main_estimate_learning_rate = 0.0005,
   bootstrap_learning_rate = 0.003,
   custom_model_index = update,
   new_predictor_set = F
